722f3a19bb19cf299527cff66856d585315c961a
Emmanuel Oluyemi <oluyemiemma2409@gmail.com>
2024-08-08 03:17:55 +0100

    4

--- WhatsApp/settings.py
+++ WhatsApp/settings.py
@@ -24,11 +24,10 @@ SECRET_KEY = 'django-insecure-2wo_-3#&8(gpx4=vfj(@lf*k6e@hn0kg+0)#_b31y)76lh1!rr
 
 # SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = True 
-
-ALLOWED_HOSTS = [] 
+ 
 DEBUG = True
 #'192.168.43.87'
-ALLOWED_HOSTS = ['coding74whiz.pythonanywhere.com'] 
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
